b4a45db6340a5f39ccad329db804ceb4148c5ff7
unknown <van@vanocular.com>
2021-02-21 09:37:20 -0500

    All three test models rendered

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './Container.css';
 import Scene from './Scene';
 import Title from './Title';
+import Model1 from '../assets/model1.stl';
+import Model2 from '../assets/model2.stl';
+import Model3 from '../assets/model3.stl';
 
 // !VA Using this example: https://codesandbox.io/s/github/supromikali/react-three-demo?file=/src/index.js:0-4455
 
 const contents = [
-  { id: 1, title: 'Mic Clip', stlfile: 'Model1'},
-  // { id: 2, title: 'Mixer Stand', stlfile: 'Model2'},
-  // { id: 3, title: 'Computer Riser', stlfile: 'Model3'}
+  { id: 1, title: 'Mic Clip', model: Model1},
+  { id: 2, title: 'Mixer Stand', model: Model2},
+  { id: 3, title: 'Computer Riser', model: Model3}
 ];
 // const title = 'This is the title';
 
@@ -30,10 +33,9 @@ class Container extends React.Component {
   render() {
       const {isMounted = true} = this.state;
 
-      const renderedContents = contents.map(({ id, title}) => {
+      const renderedContents = contents.map(({ id, title, model}) => {
         return ( 
               <div className="ui card">
-                <div className="" message={this.props.message}></div>
                 <div 
                   className="ui header large" 
                   key={id}><Title title={title} />
@@ -41,7 +43,7 @@ class Container extends React.Component {
                 <button className="btn" onClick={() => this.setState(state => ({isMounted: !state.isMounted}))}>
                   {isMounted ? "Unmount Model" : "Mount Model"}
                 </button>
-                {isMounted && <Scene />}
+                {isMounted && <Scene model={model}/>}
                 {isMounted && <div className="ui content caption">Scroll to zoom, drag to rotate</div>}
               </div>     
         );
--- src/components/Model.js
+++ src/components/Model.js
@@ -8,20 +8,23 @@ import Scene from './Scene';
 
 
 const Model = (props) => {
-  
+  console.log('props.model :>> ');
+  console.log(props.model);
+
+
   console.log('TOP OF MODEL props :>> ');
   console.log(props);
   let bbox;
   const scene = props.scene;
   const controls = props.controls;
-  console.log('HERE scene :>> ');
-  console.log(scene);
+  const camera = props.camera;
+  const model = props.model;
 
   function addSTLObject()  {
     // console.log('this.props.message Mark3 :>> ');
     // console.log(this.props.message);
     const loader = new STLLoader();
-    const promise = loader.loadAsync(Model1);
+    const promise = loader.loadAsync(model);
     promise.then( ( geometry ) => {
       // console.log('geometry :>> ');
       // console.log(geometry);
@@ -31,9 +34,14 @@ const Model = (props) => {
       geometry.center();
       mesh.geometry.computeBoundingBox();
       let bbox = mesh.geometry.boundingBox;
-      // console.log('bbox :>> ');
-      // console.log(bbox);
+      console.log('bbox :>> ');
+      console.log(bbox);
       mesh.position.set( 0, bbox.max.y, 0 );
+
+      // // !VA Set the camera.position and controls.target relative to the bounding box values. For now, the camera position is set to be 3X as far away as the dimension of the bounding box except for the z axis, which is 10 times away, since the max-z is now only 12.5 since I rotated the native model orientation. We need a better formula for that. controls.target points the camera at the 3D position, in this case, halfway up the height of the STL model.
+      camera.position.set(bbox.max.x * 3, bbox.max.y * 3, bbox.max.z * 10); 
+      controls.target = new THREE.Vector3(0, bbox.max.y, 0);
+      controls.update();
       mesh.castShadow = true;
       mesh.receiveShadow = true;
       // !VA Since this function runs twice - the first time scene is null, the second time it gets the value from the Scene component and renders. Not sure why
