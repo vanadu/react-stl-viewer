9569f2ea78e4640158e5c5ff5e7eb28d936d8a25
unknown <van@vanocular.com>
2021-02-23 05:03:15 -0500

    Session 18 complete, refactored Model component into Scene component and removed Model

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -27,7 +27,6 @@ import Model from './Model';
 let bbox;
 let scene;
 let canvas;
-let stl;
 
 
 class Scene extends Component {
@@ -35,26 +34,21 @@ class Scene extends Component {
 
 
   // !VA Originally in the Container component, pertains to the Unmount button
-  state = {isMounted: true, scene: null, camera: null, controls: null, model: 'init' };
-
+  // state = {isMounted: true, scene: null, camera: null, controls: null, model: 'init', stl: null };
+  state = {isMounted: true };
+  
   componentDidMount() {
-      // this.addSTLObject();
-      // !VA Get the scene and controls from sceneSetup and destructure
-      const [ scene, camera, controls ] = this.sceneSetup();
-      this.setState({scene: scene, camera: camera, controls: controls, model: this.props.model })
-      this.addCustomSceneObjects();
-      this.startAnimationLoop();
-      window.addEventListener('resize', this.handleWindowResize);
-      console.log('componentDidMount running');
-
-      console.log('this.scene.children :>> ');
-      console.log(this.scene.children);
-      let cldrn = this.scene.children;
-      console.log('cldrn.length :>> ' + cldrn.length);
-      for (let i = 0; i < cldrn.length; i++) {
-        console.log('cldrn[i] :>> ');
-        console.log(cldrn[i]);
-      }
+    // this.addSTLObject();
+    // !VA Get the scene and controls from sceneSetup and destructure
+    this.sceneSetup();
+    // !VA The commented lines below were required for the separate Model component
+    // const [ scene, camera, controls ] = this.sceneSetup();
+    // this.setState({scene: scene, camera: camera, controls: controls, model: this.props.model });
+    // this.setState({scene: scene, camera: camera, controls: controls });
+    this.addSTLObject();
+    this.addCustomSceneObjects();
+    this.startAnimationLoop();
+    window.addEventListener('resize', this.handleWindowResize);
 
   }
 
@@ -102,6 +96,122 @@ class Scene extends Component {
       canvas.classList.add('canv1');
       return [this.scene, this.camera, this.controls];
   };
+
+
+  addSTLObject = () => {
+    console.log('this.props.model :>> ');
+    console.log(this.props.model);
+    const loader = new STLLoader();
+    const promise = loader.loadAsync(this.props.model);
+    console.log('promise :>> ');
+    console.log(promise);
+    promise.then( ( geometry ) => 
+    {
+
+      const stlmaterial = new THREE.MeshPhongMaterial( { color: 0x007fff, specular: 0x111111, shininess: 100, fog: false } );
+      const stl = new THREE.Mesh( geometry, stlmaterial );
+
+      stl.name = 'STL';
+      console.log('stl.name :>> ' + stl.name);
+      // !VA Center the stl geometry in the scene. Required.
+      geometry.center();
+      // !VA Get the STL model's bounding box. Required for positioning it in 3D space. 
+      stl.geometry.computeBoundingBox();
+      // !VA Get the STL model's boundingSphere. Required for sizing the Sphere object used for calculating the initial camera position. 
+      stl.geometry.computeBoundingSphere();
+      // !VA get the bounding box of the stl mesh
+      let stlbox = stl.geometry.boundingBox;
+      // !VA Set the initial position of the stl mesh to set its Y position to half its height. This sets its lowest point on the grid plane.
+      stl.position.set( 0, stlbox.max.y, 0 );
+
+      // // !VA This is where we used to set the camera position. That has been replaced by the zoomExtents function, but I'm leaving the original code commented here for posterity.
+      // camera.position.set(stlbox.max.x * 3, stlbox.max.y * 3, stlbox.max.z * 10); 
+      // controls.target = new THREE.Vector3(0, stlbox.max.y, 0);
+      // controls.update();
+      // !VA Put a shadow on the STL model
+      stl.castShadow = true;
+      stl.receiveShadow = true;
+      // !VA If the Model component doesn't get the scene from the Scene component, the rest will fail silently, so add the error condition. This should be a try/catch. 
+      if (this.scene) {
+        this.scene.add(stl);
+      } else {
+        console.log('Scene does not exist');
+      }
+
+      // !VA Add a bounding sphere around the STL model to use with zoomExtents. The sphere and its material (otherwise it appears with the default opaque material) are required, but set the opacity to 0 to hide it.
+
+      var geometry2 = new THREE.SphereGeometry(stl.geometry.boundingSphere.radius, 32, 32);
+      var spherematerial = new THREE.MeshBasicMaterial({
+        color: 0xffff00
+      });
+      spherematerial.transparent = true;
+      spherematerial.opacity = 0;
+      spherematerial.opacity = 0.35;
+      var sphere = new THREE.Mesh(geometry2, spherematerial);
+      sphere.name = 'Sphere';
+      // !VA Set the sphere, as with the STL mesh, half the height of the STL mesh vertically off the grid
+      sphere.position.y = stlbox.max.y;
+      this.scene.add(sphere);
+      // !VA Run zoomExtents to position the camera based on the STL model's bounding sphere
+      // zoomExtents(this.camera, sphere, this.controls, stlbox );
+    }).catch(err => { console.log(' STL file not loaded!');});
+    
+    function failureCallback(){
+      console.log('Could not load STL file!');
+    }
+
+    
+    function zoomExtents( camera, sphere, controls, stlbox ) {
+      console.log('zoomExtents running');
+      console.log('camera :>> ');
+      console.log(camera);
+
+      // !VA These are the calcs provided by Alex Khoroshylov
+      let vFoV = camera.getEffectiveFOV();
+      let hFoV = camera.fov * camera.aspect;
+      let FoV = Math.min(vFoV, hFoV);
+      let FoV2 = FoV / 2;
+
+      let dir = new THREE.Vector3();
+      camera.getWorldDirection(dir);
+      let cameraDir = new THREE.Vector3();
+      camera.getWorldDirection(cameraDir);
+      sphere.geometry.computeBoundingSphere();
+      let bs = sphere.geometry.boundingSphere;
+      let bsWorld = bs.center.clone();
+      
+      sphere.localToWorld(bsWorld);
+
+      let th = FoV2 * Math.PI / 180.0;
+      let sina = Math.sin(th);
+      let R = bs.radius;
+      let FL = R / sina;
+
+      let cameraOffs = cameraDir.clone();
+      cameraOffs.multiplyScalar(-FL);
+
+      // !VA Add the height of the STL mesh to the camera's Z offset to draw the camera back a tad.
+      cameraOffs.z = cameraOffs.z + stlbox.max.y;
+      // !VA Set the camera position to the offset position above.
+      let newCameraPos = bsWorld.clone().add(cameraOffs);
+      // !VA Set the camera position
+      camera.position.copy(newCameraPos);
+      // !VA Move the world coordinates of the boundingSphere up accordingly. These will be the coordinates passed to lookAt to make sure the initial camera view angle matches the orbit target.
+      bsWorld.y = stlbox.max.y;
+      // !VA Set the camera lookAt to the boundingSphere world coordinates above
+      camera.lookAt(bsWorld);
+      // !VA Copy the lookAt coordinates above to the Orbit controls target so both the camera and the orbit controls are looking at the same point. 
+      controls.target.copy(bsWorld);
+      // !VA Update the orbit and camera controls.
+      controls.target.update();
+      camera.position.update();
+    }
+
+  }
+
+
+
+
 
   // Here should come custom code.
   // Code below is taken from Three.js BoxGeometry example
@@ -189,7 +299,7 @@ class Scene extends Component {
       // }
       return (
         <>
-          <Model scene={this.state.scene} camera={this.state.camera} controls={this.state.controls} model={this.state.model} />
+          {/* <Model scene={this.state.scene} camera={this.state.camera} controls={this.state.controls} model={this.state.model} /> */}
           <div className="model"
             // !VA Branch: 022221 
             // style={style} 
